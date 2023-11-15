6aa7c94ff58b2a0d05b6c1b31437eb4850ac2ae4
Dk4LL <dk4ll@proton.me>
2023-11-15 20:16:08 -0300

    :white_check_mark: LSP javascript... ok!

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,20 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+--
+-- Arquivo com a configuração dos meus autocomandos pessoais
+-- Valdigleis (Dk4LL)
+-- dk4ll@proton.me
+--
+
+-- Turn off paste mode when leaving insert
+--vim.api.nvim_create_autocmd("InsertLeave", {
+--  pattern = "*",
+--  command = "set nopaste",
+--})
+
+-- Desliga o destaque de aspas nos arquivos json
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,40 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--
+-- Configuração de atalhos pessoais usados por mim
+-- Valdigleis (Dk4LL)
+-- dk4ll@proton.me
+--
+
+-- Variáveis básicas usadas na configuração
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Para incrementar e decrementar valores inteiros no código
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Deletar uma palavra
+keymap.set("n", "dw", "vb_d")
+
+-- Selecionar todo o texto
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Comando para Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- Comando para inicializar uma nova tab
+keymap.set("n", "te", ":tabedit", opts)
+
+-- Comandos para mudar entre as tabs
+--keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+--keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Comandos para redimensionar as janelas
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Comandos para os diagnósticos
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
