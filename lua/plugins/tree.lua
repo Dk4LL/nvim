716fb7bf4ce77bde60771c0104b61f07e9374432
Dk4LL <dk4ll@proton.me>
2024-03-15 23:48:21 -0300

    ✨ Tree-sitter config!

---
+++ lua/plugins/tree.lua
@@ -0,0 +1,19 @@
+-- ------------------------------------------------------------------------------------------------
+-- Install/Config to tree-sitter
+-- Author: Dk4LL (dk4ll@proton.me)
+-- Date: 15/03/2024
+-- ------------------------------------------------------------------------------------------------
+
+return {
+  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", 
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "c", "css", "haskell", "html", "java", "javascript", "lua", "ocaml", "python", "typescript", "vim", "vimdoc" },
+      --sync_install = false, 
+      highlight = { enable = true },
+      indent = { enable = true },  
+    })
+  end
+}
